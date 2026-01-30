003605947c8c5a42e0e9edd5124df04c6f965483
cds <100245204+22o8@users.noreply.github.com>
2026-01-30 04:23:53 +0300

    Update Program.cs

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -92,7 +92,15 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var runMigrations =
+    config["RUN_MIGRATIONS"] ??
+    Environment.GetEnvironmentVariable("RUN_MIGRATIONS");
+
+if (string.Equals(runMigrations, "true", StringComparison.OrdinalIgnoreCase))
+{
     db.Database.Migrate();
+}
+
 }
 
 // Render / Reverse Proxy Support (مهم)
